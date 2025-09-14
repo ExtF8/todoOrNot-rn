068e23eb6b80b8f3f3acbb290610e6e6020d8b6e
ExtF8 <78138567+ExtF8@users.noreply.github.com>
2025-09-14 17:09:35 +0200

    Adjusted style and layout

--- src/ui/components/TodoRow.tsx
+++ src/ui/components/TodoRow.tsx
@@ -38,7 +38,7 @@ const styles = StyleSheet.create({
         width: '100%',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        paddingHorizontal: 20,
+        paddingHorizontal: 30,
     },
     row: {
         alignItems: 'center',
